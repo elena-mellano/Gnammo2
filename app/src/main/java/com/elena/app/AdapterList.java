3613b14aa227a2e6ed0ef5a415271ea4f7ac38b1
IvanoAlvino <ivano.alvino@gmail.com>
2014-06-29 18:24:32 +0200

    Added async loading images
    async loaded, still a problem that loads all the images in the same
    spot before displaying them correctly

--- app/src/main/java/com/elena/app/AdapterList.java
+++ app/src/main/java/com/elena/app/AdapterList.java
@@ -3,6 +3,7 @@ package com.elena.app;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -24,31 +26,61 @@ public class AdapterList extends ArrayAdapter<Event> {
                             Event[] objects) {
         super(context, textViewResourceId, objects);
     }
-    Bitmap img;
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
+        ViewHolder holder;
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) getContext()
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.row, null);
+            holder = new ViewHolder();
+            holder.title = (TextView) convertView.findViewById(R.id.text);
+            holder.owner = (TextView) convertView.findViewById(R.id.textowner);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.image);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
-        TextView title  = (TextView)convertView.findViewById(R.id.text);
-        TextView owner  = (TextView)convertView.findViewById(R.id.textowner);
-        ImageView imag  = (ImageView)convertView.findViewById(R.id.image);
         Event single = getItem(position);
-        title.setText(single.getTitle());
-        owner.setText(single.getOwner().getName());
+        holder.title.setText(single.getTitle());
+        holder.owner.setText(single.getOwner().getName());
+
+        if (holder.imageView != null) {
+            new ImageDownloaderTask(holder.imageView).execute(single.getUrlImg());
+        }
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView title;
+        TextView owner;
+        ImageView imageView;
+    }
+
+}
+
+class ImageDownloaderTask extends AsyncTask<String, Void, Bitmap> {
+    private final WeakReference imageViewReference;
+
+    public ImageDownloaderTask(ImageView imageView) {
+        imageViewReference = new WeakReference(imageView);
+    }
+
+    @Override
+    // Actual download method, run in the task thread
+    protected Bitmap doInBackground(String... params) {
+        // params comes from the execute() call: params[0] is the url.
         Bitmap bitmap=null;
-        String url=single.getUrlImg();
-        img=null;
+        Bitmap img = null;
+
         try {
-            if (url.compareTo("")!=0){
+            if (params[0].compareTo("") != 0){
                 URL urleff = null;
                 try {
-                    urleff = new URL(url);
+                    urleff = new URL(params[0]);
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
@@ -60,16 +92,25 @@ public class AdapterList extends ArrayAdapter<Event> {
         }catch (IOException e2){
             e2.printStackTrace();
         }
-
-
-
-        imag.setImageBitmap(img);
-
-        return convertView;
+        return img;
     }
 
-    public Bitmap getImage(){
-        return(img);
+    @Override
+    // Once the image is downloaded, associates it to the imageView
+    protected void onPostExecute(Bitmap bitmap) {
+        if (isCancelled()) {
+            bitmap = null;
+        }
+
+        if (imageViewReference != null) {
+            ImageView imageView = (ImageView) imageViewReference.get();
+            if (imageView != null) {
+
+                if (bitmap != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
     }
 
 }
